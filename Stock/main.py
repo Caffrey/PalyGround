f1650da5d63ef91d7d3c02a4d931c178be089b8b
钟振鹏 <zhongzhenpeng@kurogame.com>
2025-09-01 15:53:41 +0800

    Stock need to download yfinance

---
+++ Stock/main.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import pandas as pd
+import datetime as dt
+import os
+
+from yfinance.utils import auto_adjust
+
+proxy = "http://127.0.0.1:7897"
+os.environ["HTTP_PROXY"] = proxy
+os.environ["HTTPS_PROXY"] = proxy
+
+
+def download_stock_data(ticker):
+    t = yf.Ticker(ticker)
+    data = t.history(period="max",auto_adjust=False)
+    return data
+
+def save_to_csv(data,filename):
+    data.to_csv(filename)
+
+stock_data = download_stock_data("^GSPC")
+stock_data.to_csv("F:/stock_data.csv")
